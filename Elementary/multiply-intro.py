80f1e99bbed7303c76aaf54b366c09f68caa6422
smallbomb <rockon590+git@gmail.com>
2020-11-27 17:40:13 +0800

    Create multiply-intro.py

---
+++ Elementary/multiply-intro.py
@@ -0,0 +1,24 @@
+# https://py.checkio.org/en/mission/multiply-intro/
+
+'''
+  (at the top right of the mission description there always is a list of available translations)
+  
+  This is an intro mission, the purpose of which is to explain how to solve missions on CheckiO and how to get the most out of solving them. 
+  When the mission is solved, one more station become available for you, containing more complex missions.
+  
+  So this mission is the easiest one. Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+'''
+
+def mult_two(a, b):
+    # your code here
+    return a * b
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(mult_two(3, 2))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert mult_two(3, 2) == 6
+    assert mult_two(1, 0) == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
